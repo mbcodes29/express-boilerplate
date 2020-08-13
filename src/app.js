df06b90d9e42db0d241517e5eb147a27a5b36821
Queens Landing <queenslanding@queenss-mbp.attlocal.net>
2020-08-13 11:54:47 -0400

    added eslint changes

--- src/app.js
+++ src/app.js
@@ -1,35 +1,38 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
+/* eslint-disable no-console */
+'use strict';
 
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan(morganOption));
+app.use(helmet());
 const { NODE_ENV } = require('./config')
 
 + app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+  res.send('Hello, world!');
+});
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
+  let response;
   if (NODE_ENV === 'production') {
-  response = { error: { message: 'server error' } }
-    } else {
-     console.error(error)
-      response = { message: error.message, error }
-     }
-    res.status(500).json(response)
-})
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
 
-app.use(cors())
+app.use(cors());
 
-module.exports = app
+module.exports = app;
